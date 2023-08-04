3dbd063d6eefddff953ad3bb19c706a87e5fa5ec
myvpn1 <137178860+myvpn1@users.noreply.github.com>
2023-08-05 00:52:11 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -2,7 +2,7 @@
 # =========================================
 GREEN='\033[0;32m'
 RED='\033[0;31m'
-ipsaya=$(wget -qO- ipinfo.io/ip)
+IPVPS=$(wget -qO- ipinfo.io/ip)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
 data_ip="https://raw.githubusercontent.com/myvpn1/VPN/main/izin"
